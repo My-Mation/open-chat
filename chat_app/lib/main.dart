94712b1db8bbffc5bca10ff733bf957b877e1afb
Debargha Sarkar <debarghasarkar175@gmail.com>
2026-02-19 08:47:58 +0530

    first commit

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Center(child: Text("Welcome hbhh to ChatApp")),
+    );
+  }
+}
